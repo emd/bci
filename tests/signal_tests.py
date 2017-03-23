351a966438037252db165fc74ed08718d566c777
emd <emd@mit.edu>
2017-03-23 14:46:53 -0700

    Automated testing of window selection

--- tests/signal_tests.py
+++ tests/signal_tests.py
@@ -2,7 +2,7 @@ from nose import tools
 import numpy as np
 from bci.signal import (
     _closest_digitized_point, _crop, _windows,
-    trigger_time, Fs, Npts_total)
+    trigger_time, Fs, Npts_per_window, Nwindows, Npts_total)
 
 
 def test__closest_digitized_point():
@@ -61,5 +61,44 @@ def test__closest_digitized_point():
 #     pass
 # 
 # 
-# def test__windows():
-#     pass
+def test__windows():
+    # Lowest window
+    tlim = [trigger_time, trigger_time + (1000. / Fs)]
+    np.testing.assert_equal(
+        _windows(tlim),
+        np.array([0]))
+
+    # Highest window
+    tlim = np.array([Npts_total - 1000, Npts_total]) * (1. / Fs)
+    np.testing.assert_equal(
+        _windows(tlim),
+        np.array([Nwindows - 1]))
+
+    # Full set of windows
+    tlim = trigger_time + (np.array([0, Npts_total]) * (1. / Fs))
+    np.testing.assert_equal(
+        _windows(tlim),
+        np.arange(Nwindows))
+
+    # Window in middle where `tlim` *exactly* sits on boundaries
+    window = 3
+    ind_lo = window * Npts_per_window              # Lower bound, global ind
+    ind_hi = ((window + 1) * Npts_per_window) - 1  # Upper bound, global ind
+
+    t0 = trigger_time + (ind_lo / Fs)
+    tf = trigger_time + (ind_hi / Fs)
+
+    np.testing.assert_equal(
+        _windows([t0, tf]),
+        np.array([window]))
+
+    # Window in middle where `tlim` *exactly* straddles boundaries
+    np.testing.assert_equal(
+        _windows([t0 - (1. / Fs), tf]),
+        np.array([window - 1, window]))
+
+    np.testing.assert_equal(
+        _windows([t0, tf + (1. / Fs)]),
+        np.array([window, window + 1]))
+
+    return
